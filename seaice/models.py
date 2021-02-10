021860cf8c695c4e9519911094a842cbe2ee3abc
elma16 <elliott.macneil17@imperial.ac.uk>
2021-02-10 14:20:54 +0000

    shortening forcing term slightly

--- seaice/models.py
+++ seaice/models.py
@@ -40,14 +40,13 @@ class SeaIceModel(object):
     def bcs(self, space):
         return [DirichletBC(space, values, "on_boundary") for values in self.bcs_values]
 
-    def mom_equ(self, uh, hh, u1, u0, p, sigma, sigma_exp, rho, func_p, ocean_curr, geo_wind, C_a, rho_a, rho_w, C_w,
+    def mom_equ(self, uh, hh, u1, u0, p, sigma, rho, func_p, ocean_curr, geo_wind, C_a, rho_a, rho_w, C_w,
                 cor):
         def mass():
             return inner(rho * hh * (u1 - u0), p) * dx
 
         def forcing():
-            return self.timestep * inner(rho * hh * cor * as_vector([uh[1] - ocean_curr[1], ocean_curr[0] - uh[0]]),
-                                         p) * dx
+            return self.timestep * inner(rho * hh * cor * cross(as_vector([0, 0, 1]), ocean_curr), p) * dx
 
         def ocean_wind_forcing():
             return self.timestep * inner(rho_a * C_a * dot(geo_wind, geo_wind) * geo_wind + rho_w * C_w * sqrt(
@@ -56,26 +55,9 @@ class SeaIceModel(object):
         def rheo():
             return self.timestep * inner(sigma, func_p) * dx
 
-        def rheo2():
-            return self.timestep * inner(-div(sigma_exp), p) * dx
-
         return mass() - forcing() + ocean_wind_forcing() + rheo()
 
-    '''
-    mass(params.rho,h,u1,u0,p)
-    lm += timestep * inner(grad(p), sh) + inner(q, (s1 - s0) + timestep * (0.5 * e ** 2 / T * sh + (0.25 * (1 - e ** 2) / T * tr(sh) + 0.25 * P / T) * Identity(2)))) * dx
-    lm -= timestep * inner(C_w * sqrt(dot(uh - ocean_curr, uh - ocean_curr)) * (uh - ocean_curr), p) * dx(degree=3)
-    lm -= inner(q * zeta * timestep / T, ep_dot) * dx
-    '''
-
-    '''
-    mass(params.rho,hh,u1,u0,p)
-    lm -= timestep * inner(rho * hh * cor * as_vector([uh[1] - ocean_curr[1], ocean_curr[0] - uh[0]]), p) * dx
-    lm += timestep * inner(rho_a * C_a * dot(geo_wind, geo_wind) * geo_wind + rho_w * C_w * sqrt(dot(uh - ocean_curr, uh - ocean_curr)) * (ocean_curr - uh), p) * dx
-    lm += timestep * inner(sigma, grad(p)) * dx
-    '''
-
-    def trans_equ(self, h_in, a_in, uh, hh, h1, a1, h0, a0, q, r, ah, n):
+    def trans_equ(self, h_in, a_in, uh, hh, ah, h1, h0, a1, a0, q, r, n):
         def in_term(var1, var2, test):
             trial = var2 - var1
             return test * trial * dx
