15af0674ef951698ed3c9d5ae6698bfafdd55cf1
elma16 <elliott.macneil17@imperial.ac.uk>
2021-02-10 18:05:59 +0000

    Trying to find a way to make the equation composition shorter

--- seaice/equations.py
+++ seaice/equations.py
@@ -1,56 +1,98 @@
+from firedrake import *
 
-class MomentumEquation(object):
-    def __init__(self):
-        def mom_equ(self, hh, u1, u0, p, sigma, rho, func1=None, uh=None, ocean_curr=None, rho_a=None, C_a=None,
-                    rho_w=None,
-                    C_w=None, geo_wind=None):
-            # TODO make forcing cross product shorter
-            def momentum():
-                return inner(rho * hh * (u1 - u0), p) * dx
 
-            def forcing():
-                if ocean_curr is None:
-                    return 0
-                else:
-                    return inner(rho * hh * cor * as_vector([u1[1] - ocean_curr[1], ocean_curr[0] - u1[0]]), p) * dx
+class Equation(object):
+    def __init__(self, h_in, a_in, uh, hh, ah, h1, h0, a1, a0, q, r, n, timestep):
+        self.h_in = h_in
+        self.a_in = a_in
+        self.uh = uh
+        self.hh = hh
+        self.ah = ah
+        self.h1 = h1
+        self.h0 = h0
+        self.a1 = a1
+        self.a0 = a0
+        self.q = q
+        self.r = r
+        self.n = n
+        self.timestep = timestep
 
-            def stress1(density, drag):
-                if ocean_curr is None:
-                    return 0
-                else:
-                    return inner(density * drag * sqrt(dot(ocean_curr - uh, ocean_curr - uh)) * (ocean_curr - uh),
-                                 p) * dx
+    def neglect(self, term):
+        if None in locals().values():
+            return 0
+        else:
+            pass
 
-            def stress2(density, drag, func):
-                if func is None:
-                    return 0
-                else:
-                    return inner(density * drag * sqrt(dot(func, func)) * func, p) * dx
+    def assemble(self):
+        equ = 0
+        for term in Equation.__dict__.values():
+            equ += term()
+        return equ
 
-            def alt_forcing():
-                if func1 is None:
-                    return 0
-                else:
-                    return inner(func1, p) * dx
 
-            def rheo():
-                return inner(sigma, grad(p)) * dx
+class MomentumEquation(Equation):
+    def __init__(self, u1, u0, hh, uh, p, sigma, rho, h_in, a_in, ah, h1, h0, a1, a0, q, r, n, timestep, cor,
+                 ocean_curr, rho_w, rho_a, C_w, C_a, geo_wind):
+        super().__init__(h_in, a_in, uh, hh, ah, h1, h0, a1, a0, q, r, n, timestep)
+        self.u1 = u1
+        self.u0 = u0
+        self.sigma = sigma
+        self.rho = rho
+        self.p = p
+        self.cor = cor
+        self.ocean_curr = ocean_curr
+        self.rho_w = rho_w
+        self.C_w = C_w
+        self.C_a = C_a
+        self.rho_a = rho_a
+        self.geo_wind = geo_wind
 
-            return momentum() + self.timestep * (
-                    alt_forcing() - forcing() - stress1(rho_w, C_w) - stress2(rho_a, C_a, geo_wind) - rheo())
+    # TODO make forcing cross product shorter
+    def momentum(self):
+        return inner(self.rho * self.hh * (self.u1 - self.u0), self.p) * dx
 
-class TransportEquation(object):
-    def __init__(self):
-        def trans_equ(self, h_in, a_in, uh, hh, ah, h1, h0, a1, a0, q, r, n):
-            def in_term(var1, var2, test):
-                trial = var2 - var1
-                return test * trial * dx
+    def forcing(self):
+        return inner(self.rho * self.hh * self.cor * as_vector(
+            [self.u1[1] - self.ocean_curr[1], self.ocean_curr[0] - self.u1[0]]), self.p) * dx
 
-            def upwind_term(var1, bc_in, test):
-                un = 0.5 * (dot(uh, n) + abs(dot(uh, n)))
-                return self.timestep * (var1 * div(test * uh) * dx
-                                        - conditional(dot(uh, n) < 0, test * dot(uh, n) * bc_in, 0.0) * ds
-                                        - conditional(dot(uh, n) > 0, test * dot(uh, n) * var1, 0.0) * ds
-                                        - (test('+') - test('-')) * (un('+') * ah('+') - un('-') * var1('-')) * dS)
+    def ocean_stress(self):
+        return inner(self.rho_w * self.C_w * sqrt(dot(self.ocean_curr - self.uh, self.ocean_curr - self.uh)) * (
+                self.ocean_curr - self.uh), self.p) * dx
 
-            return in_term(h0, h1, q) + in_term(a0, a1, r) + upwind_term(hh, h_in, q) + upwind_term(ah, a_in, r)
+    def wind_stress(self):
+        return inner(self.rho_a * self.C_a * sqrt(dot(self.geo_wind, self.geo_wind)) * self.geo_wind, self.p) * dx
+
+    def rheo(self):
+        return inner(self.sigma, grad(self.p)) * dx
+
+
+class TransportEquation(Equation):
+    def __init__(self, h_in, a_in, uh, hh, ah, h1, h0, a1, a0, q, r, n, timestep):
+        super().__init__(h_in, a_in, uh, hh, ah, h1, h0, a1, a0, q, r, n, timestep)
+
+    def in_term(self, var1, var2, test):
+        trial = var2 - var1
+        return test * trial * dx
+
+    def upwind_term(self, var1, bc_in, test):
+        un = 0.5 * (dot(uh, n) + abs(dot(uh, n)))
+        return self.timestep * (var1 * div(test * uh) * dx
+                                - conditional(dot(uh, n) < 0, test * dot(uh, n) * bc_in, 0.0) * ds
+                                - conditional(dot(uh, n) > 0, test * dot(uh, n) * var1, 0.0) * ds
+                                - (test('+') - test('-')) * (un('+') * ah('+') - un('-') * var1('-')) * dS)
+
+    def assemble(self):
+        return self.in_term(self.h0, self.h1, self.q) + self.in_term(self.a0, self.a1, self.r) + self.upwind_term(
+            self.hh, self.h_in, self.q) + self.upwind_term(self.ah, self.a_in, self.r)
+
+
+x = Equation(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+
+functions = sorted([
+    getattr(x, field) for field in dir(x)
+    if hasattr(getattr(x, field), "order")
+],
+    key=(lambda field: field.order)
+)
+for func in functions:
+    func()
