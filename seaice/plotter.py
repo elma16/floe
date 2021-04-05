3a82a768690811f08ee1776e4dec1e25bd61d68f
elma16 <elliott.macneil17@imperial.ac.uk>
2021-04-05 13:28:13 +0100

    fixing plotting whilst simultaneously making it ugly

--- seaice/plotter.py
+++ seaice/plotter.py
@@ -19,14 +19,21 @@ class Plotter(object):
         self.yaxis = dataset.variables[diagnostic][:]
         dataset.close()
 
-    def plot(self,plot=plot):
+    def plot(self,plot_option='plot'):
         
         '''
         plot :: choose what plot you want to make
         '''
-        
+
         t = np.arange(0, self.timescale, self.timestep)
-        plt.plot(t, self.yaxis, label="timescale = {}".format(self.timescale))
+        if plot_option == 'plot':
+            plt.plot(t, self.yaxis, label="timescale = {}".format(self.timescale))
+        elif plot_option == 'loglog':
+            plt.loglog(t, self.yaxis, label="timescale = {}".format(self.timescale))
+        elif plot_option == 'semilogy':
+            plt.semilogy(t, self.yaxis, label="timescale = {}".format(self.timescale))
+        elif plot_option == 'semilogx':
+            plt.semilogx(t, self.yaxis, label="timescale = {}".format(self.timescale))
         plt.ylabel(r'{} of solution'.format(self.yaxis))
         plt.xlabel(r'Time [s]')
         plt.title(self.title)
