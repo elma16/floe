6a369ba03ce9df7a41165caa1860f0fd3389b10a
elma16 <elliott.macneil17@imperial.ac.uk>
2021-04-08 15:07:25 +0100

    adding the named files back

---
+++ examples/evp-stress.py
@@ -0,0 +1,53 @@
+from seaice import *
+from firedrake import *
+from pathlib import Path
+
+path = "./output/evp_stress"
+Path(path).mkdir(parents=True, exist_ok=True)
+
+# TEST 2 : EVP
+
+timestep = 20
+dumpfreq = 10 ** 3
+timescale = timestep * dumpfreq
+
+title = "EVP Plot"
+diagnostic_dirname = path + "/evp.nc"
+plot_dirname = path + "/evp_energy.png"
+
+number_of_triangles = 35
+length = 5 * 10 ** 5
+mesh = SquareMesh(number_of_triangles, number_of_triangles, length)
+x, y = SpatialCoordinate(mesh)
+
+ocean_curr = as_vector([0.1 * (2 * y - length) / length, -0.1 * (length - 2 * x) / length])
+
+ic =  {'u' : 0,'a' :  x / length, 'h':1},
+stabilised = {'state':True, 'alpha':10},
+timestepping = TimesteppingParameters(timescale=timescale, timestep=timestep)
+conditions = Conditions(stabilised=stabilised,ocean_curr=ocean_curr,ic=ic)
+
+dirname = path + "/u_timescale={}_timestep={}_stab={}.pvd".format(timescale, timestep, conditions.stabilised)
+output = OutputParameters(dirname=dirname, dumpfreq=dumpfreq)
+solver = SolverParameters()
+params = SeaIceParameters()
+
+evps = ElasticViscousPlasticStress(mesh=mesh, conditions=conditions, timestepping=timestepping, output=output, params=params,
+                                   solver_params=solver)
+
+diag = OutputDiagnostics(description="EVP Matrix Test", dirname=diagnostic_dirname)
+
+t = 0
+
+while t < timescale - 0.5 * timestep:
+    evps.solve(evps.usolver, evps.ssolver)
+    evps.update(evps.u0, evps.u1)
+    diag.dump(evps.u1, t)
+    evps.dump(evps.u1, t=t)
+    t += timestep
+    evps.progress(t)
+
+plotter = Plotter(dataset_dirname=diagnostic_dirname, diagnostic='energy', plot_dirname=plot_dirname,
+                  timestepping=timestepping, title=title)
+
+plotter.plot()
---
+++ examples/strain-rate-tensor.py
@@ -0,0 +1,67 @@
+import sys
+from seaice import *
+from firedrake import *
+from time import time
+from pathlib import Path
+
+path = "./output/srt"
+Path(path).mkdir(parents=True, exist_ok=True)
+
+# TEST 1 : STRAIN RATE TENSOR
+
+if '--test' in sys.argv:
+    timestep = 10 ** (-6)
+    dumpfreq = 10 ** 5
+    timescale = 10
+else:
+    timestep = 1
+    dumpfreq = 1
+    timescale = 10
+
+dirname = path + "/u_timescale={}_timestep={}_CR_mesh_new.pvd".format(timescale, timestep)
+title = "Test Plot"
+diagnostic_dirname = path + "/strain_rate_T={}_t={}.nc".format(timescale, timestep)
+plot_dirname = path + "/strain_rate_error_T={}_t={}.png".format(timescale, timestep)
+
+number_of_triangles = 35
+length = 5 * 10 ** 5
+mesh = PeriodicSquareMesh(number_of_triangles, number_of_triangles, length, "x")
+Vc = mesh.coordinates.function_space()
+x, y = SpatialCoordinate(mesh)
+f = Function(Vc).interpolate(as_vector([x + 0.5 * y, y]))
+mesh.coordinates.assign(f)
+
+pi_x = pi / length
+v_exp = as_vector([-sin(pi_x * x), -sin(pi_x * x)])
+
+ic = {'u': v_exp, 'a' : 1, 'h' : 1},
+conditions = Conditions(ic=ic, steady_state=True)
+timestepping = TimesteppingParameters(timescale=timescale, timestep=timestep)
+output = OutputParameters(dirname=dirname, dumpfreq=dumpfreq)
+solver = SolverParameters()
+params = SeaIceParameters()
+
+srt = ViscousPlastic(mesh=mesh, conditions=conditions, timestepping=timestepping, output=output, params=params,
+                     solver_params=solver)
+
+diag = OutputDiagnostics(description="test 1", dirname=diagnostic_dirname)
+
+t = 0
+
+w = Function(srt.V).interpolate(v_exp)
+start = time()
+while t < timescale - 0.5 * timestep:
+    srt.solve(srt.usolver)
+    srt.update(srt.u0, srt.u1)
+    diag.dump(srt.u1, t, v_exp)
+    srt.dump(srt.u1, w, t=t)
+    t += timestep
+    srt.progress(t)
+    print(Error.compute(srt.u1, w))
+end = time()
+print(end - start, "[s]")
+
+plotter = Plotter(dataset_dirname=diagnostic_dirname, diagnostic='error', plot_dirname=plot_dirname,
+                  timestepping=timestepping, title=title)
+
+plotter.plot('semilogy')
